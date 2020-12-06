504d1048c02414de29b182825b5bc01f64f94e9f
ammarakran <ammar.okran@gmail.com>
2020-12-06 22:18:55 +0100

    updates

---
+++ job/__init__.py
@@ -0,0 +1,2 @@
+from pwlidar_cloud.job.job import create_map_job
+from pwlidar_cloud.job.job import create_reduce_job
---
+++ wait/__init__.py
@@ -0,0 +1,5 @@
+from pwlidar_cloud.wait.wait_storage import wait_storage
+
+ALL_COMPLETED = 1
+ANY_COMPLETED = 2
+ALWAYS = 3
