112c4193ae7c6cb03b2723c406441e47a6b37d36
ammarakran <ammar.okran@gmail.com>
2020-12-06 23:02:58 +0100

    updates

--- executor.py
+++ executor.py
@@ -7,7 +7,8 @@ from pwlidar_cloud.invoker import FunctionInvoker
 from pwlidar_cloud.storage import InternalStorage
 from pwlidar_cloud.utils import is_pywren_function, create_executor_id
 from pwlidar_cloud.config import default_config, default_logging_config, EXECUTION_TIMEOUT, extract_storage_config
-from pywren_ibm_cloud.job import create_map_job
+from pwlidar_cloud.job import create_map_job
+
 logger = logging.getLogger(__name__)
 
 
